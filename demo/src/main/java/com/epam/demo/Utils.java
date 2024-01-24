4700307f49e1893a1ff74f8d89f78badf457a3f1
Azimbek <omar.azimbek@gmail.com>
2024-01-24 22:47:09 +0600

    2.1 maven Commit

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,5 +5,21 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
+        if (args.isEmpty() || args==null) {
+            return false;
+        }
+        for (String s : args) {
+            if (s.length()==0 || s==null) {
+                return false;
+            }
+            try {
+                if (Double.parseDouble(s)<=0) {
+                    return false;
+                }
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,17 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (str.length()==0 || str==null) {
+            return false;
+        }
+        try {
+            return Double.parseDouble(str)>0;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber("99"));
     }
 }
